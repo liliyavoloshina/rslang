2d01f7d6e4386c01eba3a7e871335bd003f78f5b
elenaBezro <lenyka_86@mail.ru>
2022-02-14 19:54:03 +0100

    feat: add timer animation

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,40 +1,17 @@
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect } from 'react'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import { useTranslation } from 'react-i18next'
 
-import { Box, Typography } from '@mui/material'
+import { Box } from '@mui/material'
+
+import { GAME_TIME, TIME_ALMOST_UP } from '~/utils/constants'
 
 import { useTimerContext } from './Timer.context'
 import { TimerProps } from './Timer.types'
 
-const Timer = ({ onTimeout, ...boxProps }: TimerProps) => {
-	const { t } = useTranslation()
+const NumberFormat = Intl.NumberFormat('ru-RU', { maximumFractionDigits: 2 })
 
+const Timer = ({ onTimeout, onTimeAlmostUp, ...boxProps }: TimerProps) => {
 	const { status, duration } = useTimerContext()
-
-	const timeLeftRef = useRef<HTMLSpanElement>(null)
-	const intervalRef = useRef<NodeJS.Timeout | undefined>()
-
-	// eslint-disable-next-line consistent-return
-	useEffect(() => {
-		if (intervalRef.current) {
-			clearInterval(intervalRef.current)
-			intervalRef.current = undefined
-		}
-
-		if (status === 'running') {
-			const startTime = new Date().getTime()
-			intervalRef.current = setInterval(() => {
-				if (timeLeftRef.current) {
-					const timePassed = (new Date().getTime() - startTime) / 1000
-					const timeLeft = Math.max(Math.floor(duration! - timePassed), 0)
-					timeLeftRef.current.textContent = t('TIMER.TIME_LEFT', { count: timeLeft })
-				}
-			}, 200)
-
-			return () => intervalRef.current && clearInterval(intervalRef.current)
-		}
-	}, [duration, status, t])
 
 	useEffect(() => {
 		if (status === 'timeout') {
@@ -42,9 +19,18 @@ const Timer = ({ onTimeout, ...boxProps }: TimerProps) => {
 		}
 	}, [onTimeout, status])
 
+	const onUpdate = useCallback(
+		(timeLeft: number) => {
+			if (timeLeft > 0 && timeLeft <= TIME_ALMOST_UP) {
+				onTimeAlmostUp?.(timeLeft)
+			}
+		},
+		[onTimeAlmostUp]
+	)
+
 	return (
 		<Box {...boxProps}>
-			<CountdownCircleTimer isPlaying duration={60} colors={['#004777', '#F7B801', '#A30000', '#A30000']} colorsTime={[60, 50, 20, 0]} size={100}>
+			<CountdownCircleTimer isPlaying duration={GAME_TIME} colors={['#004777', '#F7B801', '#A30000', '#A30000']} colorsTime={[60, 50, 20, 0]} size={100} onUpdate={onUpdate}>
 				{({ remainingTime }) => remainingTime}
 			</CountdownCircleTimer>
 			{/* <Box>{status === 'running' && <Typography ref={timeLeftRef} />}</Box> */}
