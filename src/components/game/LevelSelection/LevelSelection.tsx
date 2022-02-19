b7e7111bf7023d6c065a7806f79573a93369348f
Liliya <liliyavoloshine@gmail.com>
2022-02-19 14:09:55 +0600

    feat: update audiocall styles, add custom components

--- src/components/game/LevelSelection/LevelSelection.tsx
+++ src/components/game/LevelSelection/LevelSelection.tsx
@@ -1,61 +1,24 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Color, SelectChangeEvent } from '@mui/material'
+import { SelectChangeEvent } from '@mui/material'
 import Box from '@mui/material/Box'
-import Button, { ButtonProps } from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import FormControl from '@mui/material/FormControl'
-import InputLabel, { InputLabelProps } from '@mui/material/InputLabel'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import MenuItem from '@mui/material/MenuItem'
-import Select, { SelectProps } from '@mui/material/Select'
 import Typography from '@mui/material/Typography'
-import { deepPurple, green, lightGreen, purple, teal } from '@mui/material/colors'
-import { styled } from '@mui/material/styles'
+import { deepPurple, green, purple, teal } from '@mui/material/colors'
 
 import { useAppDispatch } from '~/app/hooks'
+import CustomButton from '~/components/layout/CustomButton'
+import CustomLabel from '~/components/layout/CustomLabel'
+import CustomSelect from '~/components/layout/CustomSelect'
 import { toggleLevelSelection } from '~/features/audiocall'
 
 import { LevelSelectionProps } from './LevelSelection.types'
-
-interface LevelSelectionCustomProps {
-	customcolor: Color
-}
-
-const LevelSelectionLabel = styled(InputLabel)<LevelSelectionCustomProps & InputLabelProps>(({ customcolor }) => ({
-	color: '#fff',
-	'&.Mui-focused': {
-		color: customcolor[200],
-	},
-}))
-
-const LevelSelectionSelect = styled(Select)<LevelSelectionCustomProps & SelectProps>(({ customcolor }) => ({
-	color: '#fff',
-
-	'& .MuiOutlinedInput-notchedOutline': {
-		borderColor: '#fff',
-	},
-
-	'&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-		borderColor: customcolor[900],
-	},
-
-	'& .MuiSvgIcon-root': {
-		color: '#fff',
-	},
-}))
-
-const LevelSelectionButton = styled(Button)<LevelSelectionCustomProps & ButtonProps>(({ customcolor }) => ({
-	color: '#fff',
-	backgroundColor: customcolor[900],
-	border: `1px solid ${customcolor[800]}`,
-	'&:hover': {
-		backgroundColor: customcolor[700],
-	},
-}))
 
 const LevelSelection = ({ title, description, controls, type, onLevelSelected }: LevelSelectionProps) => {
 	const { t } = useTranslation()
@@ -106,20 +69,21 @@ const LevelSelection = ({ title, description, controls, type, onLevelSelected }:
 				</Box>
 				<Box display="flex" gap="20px" marginTop="20px">
 					<FormControl color="primary" variant="outlined">
-						<LevelSelectionLabel customcolor={customColor}>{t('LEVEL_SELECTION.SELECT_LEVEL')}</LevelSelectionLabel>
+						<CustomLabel customcolor={customColor}>{t('LEVEL_SELECTION.SELECT_LEVEL')}</CustomLabel>
 
-						<LevelSelectionSelect value={group} label={t('LEVEL_SELECTION.SELECT_LEVEL')} onChange={handleChange} customcolor={customColor}>
+						<CustomSelect value={group} label={t('LEVEL_SELECTION.SELECT_LEVEL')} onChange={handleChange} customcolor={customColor}>
 							<MenuItem value={0}>{t('LEVEL_SELECTION.GROUP', { count: 1 })}</MenuItem>
 							<MenuItem value={1}>{t('LEVEL_SELECTION.GROUP', { count: 2 })}</MenuItem>
 							<MenuItem value={2}>{t('LEVEL_SELECTION.GROUP', { count: 3 })}</MenuItem>
 							<MenuItem value={3}>{t('LEVEL_SELECTION.GROUP', { count: 4 })}</MenuItem>
 							<MenuItem value={4}>{t('LEVEL_SELECTION.GROUP', { count: 5 })}</MenuItem>
 							<MenuItem value={5}>{t('LEVEL_SELECTION.GROUP', { count: 6 })}</MenuItem>
-						</LevelSelectionSelect>
+						</CustomSelect>
 					</FormControl>
-					<LevelSelectionButton variant="contained" onClick={handlePlay} customcolor={customColor}>
+
+					<CustomButton variant="contained" onClick={handlePlay} customcolor={customColor}>
 						{t('LEVEL_SELECTION.PLAY')}
-					</LevelSelectionButton>
+					</CustomButton>
 				</Box>
 			</Container>
 		</Box>
