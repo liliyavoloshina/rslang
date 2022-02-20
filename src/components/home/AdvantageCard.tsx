e435ab963926dbc63835db25f31efce7549fe6e6
elenaBezro <lenyka_86@mail.ru>
2022-02-20 16:06:53 +0100

    feat: design home page

--- src/components/home/AdvantageCard.tsx
+++ src/components/home/AdvantageCard.tsx
@@ -1,36 +1,46 @@
 import { ReactNode } from 'react'
 
+import { RocketLaunch } from '@mui/icons-material'
 import AutoGraph from '@mui/icons-material/AutoGraph'
 import MenuBook from '@mui/icons-material/MenuBook'
-import School from '@mui/icons-material/School'
 import SportsEsports from '@mui/icons-material/SportsEsports'
-import { Card, CardContent, Typography } from '@mui/material'
+import { Card, CardActionArea, CardContent, Typography } from '@mui/material'
 
 interface AdvantageCardProps {
 	title: string
 	desc: string
+	cardColor: string
 	icon: string
 }
 
 const ADVANTAGE_ICONS: Record<string, ReactNode | undefined> = {
-	game: <SportsEsports sx={{ fontSize: 60 }} />,
-	book: <MenuBook sx={{ fontSize: 60 }} />,
-	school: <School sx={{ fontSize: 60 }} />,
-	progress: <AutoGraph sx={{ fontSize: 60 }} />,
+	book: <MenuBook sx={{ fontSize: 60, color: 'white' }} />,
+	rocket: <RocketLaunch sx={{ fontSize: 60, color: 'white' }} />,
+	game: <SportsEsports sx={{ fontSize: 60, color: 'white' }} />,
+	progress: <AutoGraph sx={{ fontSize: 60, color: 'white' }} />,
 }
 
-export default function AdvantageCard({ icon, title, desc }: AdvantageCardProps) {
+const ADVANTAGE_BG_COLORS: Record<string, string> = {
+	book: '#38c4c1',
+	rocket: '#feca02',
+	game: '#fe7ec9',
+	progress: '#488df4',
+}
+
+export default function AdvantageCard({ icon, title, desc, cardColor }: AdvantageCardProps) {
 	return (
-		<Card sx={{ maxWidth: 345, textAlign: 'center' }}>
-			{ADVANTAGE_ICONS[icon]}
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="div">
-					{title}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					{desc}
-				</Typography>
-			</CardContent>
+		<Card sx={{ maxWidth: 215, height: 215, textAlign: 'center', bgcolor: `${ADVANTAGE_BG_COLORS[cardColor]}`, paddingTop: 1 }}>
+			<CardActionArea>
+				{ADVANTAGE_ICONS[icon]}
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div" color="white">
+						{title}
+					</Typography>
+					<Typography variant="body2" color="white">
+						{desc}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
 		</Card>
 	)
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,54 +1,139 @@
 import { TFuncKey, useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 
-import { Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
+import { Box, Card, CardContent, CardMedia, Container, Grid, IconButton, Link, Typography, colors } from '@mui/material'
 
 import AdvantageCard from '~/components/home/AdvantageCard'
+import PhilosophyCard from '~/components/home/PhilosophyCard'
+import { Path } from '~/components/router'
 
-const advantages: { title: TFuncKey; desc: TFuncKey; icon: string }[] = [
-	{ title: 'HOME.ADVANTAGES.MINI_GAMES', desc: 'HOME.ADVANTAGES.MINI_GAMES_DESC', icon: 'game' },
-	{ title: 'HOME.ADVANTAGES.TEXTBOOX', desc: 'HOME.ADVANTAGES.TEXTBOOX_DESC', icon: 'book' },
-	{ title: 'HOME.ADVANTAGES.CHOOSE_WORDS', desc: 'HOME.ADVANTAGES.CHOOSE_WORDS_DESC', icon: 'school' },
-	{ title: 'HOME.ADVANTAGES.LEARNING_PROGRESS', desc: 'HOME.ADVANTAGES.LEARNING_PROGRESS_DESC', icon: 'progress' },
-	{ title: 'HOME.ADVANTAGES.LEARNING_PROGRESS', desc: 'HOME.ADVANTAGES.LEARNING_PROGRESS_DESC', icon: 'progress' },
-	{ title: 'HOME.ADVANTAGES.LEARNING_PROGRESS', desc: 'HOME.ADVANTAGES.LEARNING_PROGRESS_DESC', icon: 'progress' },
+const advantages: { title: TFuncKey; desc: TFuncKey; icon: string; cardColor: string; path: Path }[] = [
+	{ title: 'HOME.ADVANTAGES.TEXTBOOK', desc: 'HOME.ADVANTAGES.TEXTBOOK_DESC', icon: 'book', cardColor: 'book', path: Path.TEXTBOOK },
+	{ title: 'HOME.ADVANTAGES.SPRINT', desc: 'HOME.ADVANTAGES.SPRINT_DESC', icon: 'rocket', cardColor: 'rocket', path: Path.SPRINT },
+	{ title: 'HOME.ADVANTAGES.AUDIOCALL', desc: 'HOME.ADVANTAGES.AUDIOCALL_DESC', icon: 'game', cardColor: 'game', path: Path.AUDIOCALL },
+	{ title: 'HOME.ADVANTAGES.STATISTIC', desc: 'HOME.ADVANTAGES.STATISTIC_DESC', icon: 'progress', cardColor: 'progress', path: Path.STATISTIC },
+]
+
+const philosophy: { title: TFuncKey; desc: TFuncKey; textColor: string }[] = [
+	{ title: 'HOME.PHILOSOPHY.TEXTBOOK', desc: 'HOME.PHILOSOPHY.TEXTBOOK_DESC', textColor: 'textbook' },
+	{ title: 'HOME.PHILOSOPHY.DICTIONARY', desc: 'HOME.PHILOSOPHY.DICTIONARY_DESC', textColor: 'dictionary' },
+	{ title: 'HOME.PHILOSOPHY.STATISTIC', desc: 'HOME.PHILOSOPHY.STATISTIC_DESC', textColor: 'statistic' },
+	{ title: 'HOME.PHILOSOPHY.GAMES', desc: 'HOME.PHILOSOPHY.GAMES_DESC', textColor: 'games' },
 ]
 
 export default function Home() {
 	const { t } = useTranslation()
+	const navigate = useNavigate()
+
 	return (
-		<Container maxWidth="lg">
-			<Typography variant="h2" sx={{ mt: 3, mb: 3, textAlign: 'center' }}>
-				{t('HOME.TITLE')}
-			</Typography>
-			<Typography variant="body1" gutterBottom sx={{ mb: 5, textAlign: 'center' }}>
-				{t('HOME.DESCRIPTION')}
-			</Typography>
-			<Grid container spacing={2}>
-				{advantages.map(({ title, desc, icon }, idx) => (
-					<Grid item xs={4} key={idx}>
-						<AdvantageCard title={t(title) as string} desc={t(desc) as string} icon={icon} />
+		<>
+			<Container>
+				<Box maxWidth="lg">
+					<Grid container style={{ position: 'relative', marginTop: 40 }}>
+						<Grid item xs={5}>
+							<img style={{ width: 450, height: 490, objectFit: 'contain', borderRadius: 5 }} src="/assets/images/home_page_learning.jpg" alt="learning" />
+							<Box sx={{ width: 420, height: 430, backgroundImage: 'linear-gradient(45deg, #feca02, #fe7ec9)', margin: '-345px auto 0 75px', borderRadius: 1 }} />
+						</Grid>
+
+						<Grid item xs={7}>
+							<Typography
+								variant="h3"
+								gutterBottom
+								sx={{ mb: 5, textAlign: 'center', color: '#488df4', verticalAlign: 'center', textTransform: 'uppercase', fontWeight: 800, padding: 8 }}
+							>
+								{t('HOME.DESCRIPTION')}
+							</Typography>
+						</Grid>
+
+						<Grid container spacing={0} maxWidth={900} style={{ position: 'absolute', top: 325, right: 5 }}>
+							{advantages.map(({ title, desc, icon, cardColor, path }, idx) => (
+								<Grid item xs={3} key={idx} onClick={() => navigate(path)}>
+									<AdvantageCard title={t(title) as string} desc={t(desc) as string} icon={icon} cardColor={cardColor} />
+								</Grid>
+							))}
+						</Grid>
 					</Grid>
-				))}
-			</Grid>
-			<Typography variant="h4" sx={{ mt: 3, mb: 3, textAlign: 'center' }}>
-				{t('HOME.DEVELOPER')}
-			</Typography>
-			<Card sx={{ maxWidth: 345 }}>
-				<CardMedia
-					component="img"
-					height="140"
-					image="https://images.unsplash.com/photo-1580894908361-967195033215?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-					alt="green iguana"
-				/>
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="div">
-						{t('HOME.FIRST_DEVELOPER.NICKNAME')}
+				</Box>
+			</Container>
+			<Box sx={{ width: 'calc(100% - 80px)', bgcolor: '#38c4c1', margin: '100px auto', padding: 5 }}>
+				<Container>
+					<Typography variant="h4" sx={{ mt: 3, mb: 3, textAlign: 'center', fontWeight: 800, color: 'white' }}>
+						{t('HOME.PHILOSOPHY.TITLE')}
 					</Typography>
-					<Typography variant="body2" color="text.secondary">
-						{t('HOME.FIRST_DEVELOPER.DESCRIPTION')}
+
+					<Grid container spacing={5} sx={{ justifyContent: 'space-round' }}>
+						{philosophy.map(({ title, desc, textColor }, idx) => (
+							<Grid item xs={3} key={idx}>
+								<PhilosophyCard title={t(title) as string} desc={t(desc) as string} textColor={textColor} />
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+			</Box>
+
+			<Box position="relative" style={{ marginBottom: 80 }}>
+				<Box sx={{ position: 'absolute', width: '100%', height: 144, top: 260, background: '#488df4' }} />
+				<Container>
+					<Typography variant="h4" sx={{ mt: 3, mb: 3, textAlign: 'center', fontWeight: 800, color: '#488df4' }}>
+						{t('HOME.ABOUT_US')}
 					</Typography>
-				</CardContent>
-			</Card>
-		</Container>
+					<Grid container spacing={5} sx={{ justifyContent: 'space-between', height: 430 }}>
+						<Grid item xs={6} position="relative">
+							<Box sx={{ position: 'absolute', width: '75%', height: '100%', backgroundImage: 'linear-gradient( #feca02, #fe7ec9 )', borderRadius: 1 }} />
+							<Card sx={{ width: '75%', transform: 'translate(25%, 80px)' }}>
+								<CardMedia
+									component="img"
+									height="140"
+									width="140"
+									style={{ borderRadius: '50%', objectFit: 'contain' }}
+									image="/assets/images/lady_avatar_blond.png"
+									alt="avatar"
+								/>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="div" textAlign="center">
+										<Link href="https://github.com/liliyavoloshina" underline="hover" color="#488df4">
+											{t('HOME.FIRST_DEVELOPER.NICKNAME')}
+											{/* <img style={{ width: 20, height: 20, objectFit: 'contain' }} src="/assets/svg/github.png" alt="" /> */}
+										</Link>
+									</Typography>
+									<Typography gutterBottom variant="body1" component="div">
+										{t('HOME.FIRST_DEVELOPER.TASKS')}
+									</Typography>
+									<Typography variant="body2" color="text.secondary">
+										{t('HOME.FIRST_DEVELOPER.TASKS_DESCRIPTION')}
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={6} position="relative">
+							<Box sx={{ position: 'absolute', width: '75%', height: '100%', right: 0, backgroundImage: 'linear-gradient( #fe7ec9, #feca02 )', borderRadius: 1 }} />
+							<Card sx={{ width: '75%', transform: 'translate(10%, 25px)' }}>
+								<CardMedia
+									component="img"
+									height="140"
+									width="140"
+									style={{ borderRadius: '50%', objectFit: 'contain' }}
+									image="/assets/images/lady_avatar_brown.png"
+									alt="avatar"
+								/>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="div" textAlign="center">
+										<Link href="https://github.com/ElenaBezro" underline="hover" color="#488df4">
+											{t('HOME.SECOND_DEVELOPER.NICKNAME')}
+										</Link>
+									</Typography>
+									<Typography gutterBottom variant="body1" component="div">
+										{t('HOME.SECOND_DEVELOPER.TASKS')}
+									</Typography>
+									<Typography variant="body2" color="text.secondary">
+										{t('HOME.SECOND_DEVELOPER.TASKS_DESCRIPTION')}
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+					</Grid>
+				</Container>
+			</Box>
+		</>
 	)
 }
